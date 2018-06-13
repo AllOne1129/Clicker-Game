e09391403e2729a331e36f424d61a26addb54706
AllOne <allone1129@gmail.com>
2018-06-13 23:14:43 +0900

    #1 Finished

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,40 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
     private float _Cash;
-    public Text CashText;
     public float Cash
     {
         get { return _Cash; }
         set
         {
             _Cash = value;
-            CashText.text = "You have : $" + _Cash;
-            Cash.ToString("0.00");
+            CashText.text = "You have : $" + _Cash.ToString("0.00");
         }
     }
 
     private float _CashPerSecond;
-    public Text RateText;
     public float CashPerSecond
     {
         get { return _CashPerSecond; }
         set
         {
             _CashPerSecond = value;
-            RateText.text = "per second: " + _CashPerSecond.ToString("0.0");
+            RateText.text = "per second : " + _CashPerSecond.ToString("0.00");
         }
     }
+
+    [Tooltip("버튼을 누를 때마다 플레이어가 벌어들이는 돈.")]
+    public float CashPerClick = 1;
+
+    [Header("Object References")]
+    public Text CashText;
+    public Text RateText;
 	// Use this for initialization
 	void Start () {
-		
+        Cash = 0;
+        CashPerSecond = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Cash += CashPerSecond * Time.deltaTime;
 	}
+
+    public void ClickedButton()
+    {
+        Cash += CashPerClick;
+    }
 }
---
+++ Assets/Scripts/StoreItem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType
+{
+    ClickPower,PerSecondIncrease
+};
+
+public class StoreItem : MonoBehaviour {
+    [Tooltip("이 업그레이드의 가격이 얼만지")]
+    public int Cost;
+
+    public ItemType itemType;
+
+    [Tooltip("구매한다면 얼마나 증가될 것인지")]
+    public float IncreaseAmount;
+
+    private int qty;
+
+    public Text costText;
+    public Text qtyText;
+
+    private GameController controller;
+    private Button button;
+
+	// Use this for initialization
+	void Start () {
+        qty = 0;
+        qtyText.text = qty.ToString();
+        costText.text = "$" + Cost.ToString();
+
+        button = transform.GetComponent<Button>();
+
+        button.onClick.AddListener(this.ButtonClicked);
+
+        controller = GameObject.FindObjectOfType<GameController>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        button.interactable = (controller.Cash >= Cost);
+	}
+
+    public void ButtonClicked()
+    {
+        controller.Cash -= Cost;
+        switch(itemType)
+        {
+            case ItemType.ClickPower:
+                controller.CashPerClick += IncreaseAmount;
+                break;
+            case ItemType.PerSecondIncrease:
+                controller.CashPerSecond += IncreaseAmount;
+                break;
+        }
+
+        qty++;
+        qtyText.text = qty.ToString();
+    }
+}
